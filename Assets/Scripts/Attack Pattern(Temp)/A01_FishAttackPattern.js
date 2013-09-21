e8047b7900a0bbfe3484aacaf4e7773eea2269ba
suwaki <jo.ak97@yahoo.com.hk>
2013-09-21 13:04:03 +0800

    Danmaku Testing -a new danmaku is set to test how to write the script.

--- Assets/Scripts/Attack Pattern(Temp)/A01_FishAttackPattern.js
+++ Assets/Scripts/Attack Pattern(Temp)/A01_FishAttackPattern.js
@@ -7,38 +7,42 @@
 var Bullet : Rigidbody;
 var PowerZ = 10.0;
 var ThrowerSound : AudioClip;
+var startTime = 0.0;
+var lastTime = 0.0;
+var j = 0;
 
 /* 功能 : 持續執行 (投射) */
 //如果發射狀態為(是) --> 當我們按下開火鍵(Fire1)時 -->於腳本附加的物件上動態產生一把斧頭 -->
  //給予方向及力量將其投射出去 --> 避開斧頭與自身的物理碰撞現象 --> 播放投射聲音。
-function Update () 
+function Awake(){
+	startTime = Time.time;
+	j = 0;
+}
+function FixedUpdate () 
 {
 	//if(_11_Trigger_MagicStage.FireStatus == true)
 	//{
-		if(Time.frameCount%2==0)
+	
+		if((Time.time-lastTime)>1/20.0)
 		{
 			var temp = Mathf.Sin(Time.frameCount/50.0);
 			var angle = (temp*1640.0 +90.0)/180.0*Mathf.PI;
 			var result = PowerZ;
-			
-			var AxeA : Rigidbody = Instantiate(Bullet, transform.position, transform.rotation);
-			AxeA.velocity = transform.TransformDirection( Vector3(result*Mathf.Cos(angle), 0, result*Mathf.Sin(angle)) );
-			AxeA.useGravity = false;
-			
-			AxeA = Instantiate(Bullet, transform.position, transform.rotation);
-			AxeA.velocity = transform.TransformDirection( Vector3(-result*Mathf.Cos(angle), 0, -result*Mathf.Sin(angle)) );
-			AxeA.useGravity = false;
+			var BulletX : Rigidbody;
+			for(i=0;i<6;i++){
+				angle = Mathf.PI/36.0*(i*6+j) + j/100.0;
+				BulletX = Instantiate(Bullet, transform.position, transform.rotation);
+				BulletX.gameObject.AddComponent("A01_B1");
+				BulletX.gameObject.GetComponent("A01_B1").startTime = Time.time;
+				BulletX.gameObject.GetComponent("A01_B1").vx = result * Mathf.Cos(angle);
+				BulletX.gameObject.GetComponent("A01_B1").vz = result * Mathf.Sin(angle);
+				BulletX.gameObject.GetComponent("A01_B1").oriPos = transform.position;
+				BulletX.useGravity = false;
+				j++;
+			}
+			lastTime = Time.time;
     		//AxeA.detectCollisions = false;
     		
-    		result /= 2.0;
-    		var AxeB : Rigidbody = Instantiate(Bullet, transform.position, transform.rotation);
-			AxeB.velocity = transform.TransformDirection( Vector3(result*Mathf.Sin(angle), 0, result*Mathf.Cos(angle)) );
-			AxeB.useGravity = false;
-			
-			AxeB  = Instantiate(Bullet, transform.position, transform.rotation);
-			AxeB.velocity = transform.TransformDirection( Vector3(-result*Mathf.Sin(angle), 0, -result*Mathf.Cos(angle)) );
-			AxeB.useGravity = false;
-    		//AxeB.detectCollisions = false;
 			//audio.PlayOneShot(ThrowerSound);
 		}
 	//}
