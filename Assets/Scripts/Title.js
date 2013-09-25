641b659e8a12f23f50faf95a72163002b25b6051
CodeMonkey <louischan0325@gmail.com>
2013-09-25 22:02:10 +0800

    a very simple title screen that does nothing

---
+++ Assets/Scripts/Title.js
@@ -0,0 +1,36 @@
+﻿var texture : Texture;
+
+var skin : GUISkin;
+
+
+function OnGUI() {
+	GUI.skin = skin;
+
+	GUILayout.Box(texture);
+	
+	var areaWidth = Screen.width / 3;
+	var areaHeight = Screen.height * 0.8;
+	var areaX = Screen.width / 2 - areaWidth / 2;
+	var areaY = Screen.height * 0.2;
+	
+	GUILayout.BeginArea(Rect(areaX, areaY, areaWidth, areaHeight));
+	GUILayout.BeginVertical();
+	
+	if (GUILayout.Button("Start Game")) {
+		print("game started");
+	}
+	if (GUILayout.Button("Load Game")) {
+		print("game loaded");
+	}
+	if (GUILayout.Button("Option")) {
+		print("go to options screen");
+	}
+	if (GUILayout.Button("Exit")) {
+		// This call is ignored in editor
+		// only functions in builds
+		Application.Quit();
+	}
+	
+	GUILayout.EndVertical();
+	GUILayout.EndArea();
+}
