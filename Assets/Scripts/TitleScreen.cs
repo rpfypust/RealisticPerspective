fe77e4f8fa04d51aa3927ad5c8743a7aef235e69
CodeMonkey <louischan0325@gmail.com>
2013-09-30 22:21:48 +0800

    make sure that the gui matrix is preserved after OnGUI calls

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -4,47 +4,54 @@ using System.Collections;
 public class TitleScreen : MonoBehaviour {
 	
 	public Texture2D backgroundImage;
-	public GUISkin skin;
 	
-	private int optimalWidth = 1280;
-	private int optimalHeight = 720;
+	private int nativeWidth = 1280;
+	private int nativeHeight = 720;
 	private Vector3 scaleVector;
 	private int fontSize;
 	
 	void Start() {
-		float scaleFactor = ((float) Screen.height) / optimalHeight;
+		float widthRatio = ((float) Screen.width) / nativeWidth;
+		float heightRatio = ((float) Screen.height) / nativeHeight;
+		float scaleFactor = (widthRatio > heightRatio) ? heightRatio : widthRatio;
 		scaleVector = new Vector3(scaleFactor, scaleFactor, 1.0f);
-		fontSize = Mathf.RoundToInt(optimalHeight * 0.04f);
+		fontSize = Mathf.RoundToInt(nativeHeight * 0.04f);
 	}
 	
 	void OnGUI() {
+		// backup the matrix
+		Matrix4x4 backupMatrix = GUI.matrix;
+		
 		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scaleVector);
-		GUI.skin = skin;
-		skin.button.fontSize = fontSize;
+		GUIStyle reSizeFont = new GUIStyle(GUI.skin.button);
+		reSizeFont.fontSize = fontSize;
 		
 		// Draw the background image
-		GUI.DrawTexture(new Rect(0, 0, optimalWidth, optimalHeight), backgroundImage, ScaleMode.StretchToFill);
+		GUI.DrawTexture(new Rect(0, 0, nativeWidth, nativeHeight), backgroundImage, ScaleMode.StretchToFill);
 		
-		int areaWidth = Mathf.RoundToInt(optimalWidth / 3.0f);
-		int areaHeight = Mathf.RoundToInt(optimalHeight * 0.6f);
-		int areaX = optimalWidth / 2 - areaWidth / 2;
-		int areaY = Mathf.RoundToInt(optimalHeight * 0.2f);
+		int areaWidth = Mathf.RoundToInt(nativeWidth / 3.0f);
+		int areaHeight = Mathf.RoundToInt(nativeHeight * 0.6f);
+		int areaX = nativeWidth / 2 - areaWidth / 2;
+		int areaY = Mathf.RoundToInt(nativeHeight * 0.2f);
 		
 		GUILayout.BeginArea(new Rect(areaX, areaY, areaWidth, areaHeight));
 		GUILayout.BeginVertical();
-		if (GUILayout.Button("Play Game", GUILayout.ExpandHeight(true))) {
+		if (GUILayout.Button("Play Game", reSizeFont, GUILayout.ExpandHeight(true))) {
 			Application.LoadLevel("testing_use");
 		}
-		if (GUILayout.Button("Load Game", GUILayout.ExpandHeight(true))) {
+		if (GUILayout.Button("Load Game", reSizeFont, GUILayout.ExpandHeight(true))) {
 			Debug.Log("game loaded");
 		}
-		if (GUILayout.Button("Options", GUILayout.ExpandHeight(true))) {
+		if (GUILayout.Button("Options", reSizeFont, GUILayout.ExpandHeight(true))) {
 			Debug.Log("options");
 		}
-		if (GUILayout.Button("Quit", GUILayout.ExpandHeight(true))) {
+		if (GUILayout.Button("Quit", reSizeFont, GUILayout.ExpandHeight(true))) {
 			Application.Quit();
 		}
 		GUILayout.EndVertical();
 		GUILayout.EndArea();
+		
+		// restore the matrix
+		GUI.matrix = backupMatrix;
 	}
 }
