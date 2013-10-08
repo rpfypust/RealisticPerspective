8e3a28c416802c296e68c2750e947909550e8a22
CodeMonkey <louischan0325@gmail.com>
2013-10-08 14:12:48 +0800

    separate the FPS monitor from old character control script

---
+++ Assets/Scripts/FPSMonitor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSMonitor : MonoBehaviour
+{
+
+	void OnGUI()
+	{
+		Vector2 nameSize;
+	
+		string showString = Mathf.Round(1.0f / Time.deltaTime).ToString() + "fps";
+		nameSize = GUI.skin.label.CalcSize(new GUIContent(showString));
+		GUI.color = Color.red;
+		GUI.Label(new Rect(0.0f, 0.0f, nameSize.x, nameSize.y), showString);
+	
+		showString = Time.frameCount.ToString() + " frames passed";
+		nameSize = GUI.skin.label.CalcSize(new GUIContent(showString));
+		GUI.color = Color.red;
+		GUI.Label(new Rect(0.0f, nameSize.y, nameSize.x, nameSize.y), showString);
+	
+		showString = Mathf.Round(1000.0f * Time.time).ToString() + " seconds passed";
+		nameSize = GUI.skin.label.CalcSize(new GUIContent(showString));
+		GUI.color = Color.red;
+		GUI.Label(new Rect(0.0f, nameSize.y * 2.0f, nameSize.x, nameSize.y), showString);
+	
+		showString = Mathf.Round(Time.frameCount / Time.time).ToString() + "fps";
+		nameSize = GUI.skin.label.CalcSize(new GUIContent(showString));
+		GUI.color = Color.red;
+		GUI.Label(new Rect(0.0f, nameSize.y * 3.0f, nameSize.x, nameSize.y), showString);
+	}
+}
