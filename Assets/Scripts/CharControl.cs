6f2e7b4f07e82d8097492d33d55c06c444433611
CodeMonkey <louischan0325@gmail.com>
2014-01-01 23:11:10 +0800

    fixed the bug that player will rotate even he is not moving

--- Assets/Scripts/CharControl.cs
+++ Assets/Scripts/CharControl.cs
@@ -29,8 +29,11 @@ public class CharControl : MonoBehaviour
 		moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
 		
 		// Rotate to face forward
-		if (moveDirection != Vector3.zero && !Input.GetButton ("Slow"))
-			transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(new Vector3(moveDirection.x, 0.0f, moveDirection.z)), turnSpeed * Time.deltaTime);
+		if (!Input.GetButton ("Slow") && (Input.GetButton("Horizontal") || Input.GetButton("Vertical"))) {
+			Vector3 facingDirection = new Vector3(moveDirection.x, 0.0f, moveDirection.z);
+			if (facingDirection != Vector3.zero) 
+				transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(facingDirection), turnSpeed * Time.deltaTime);
+		}
 		
 		if (controller.isGrounded) {
 			moveSpeed = (Input.GetButton("Slow"))? slowSpeed : walkSpeed;
