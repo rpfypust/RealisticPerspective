d6aef419340dee958108d1d45d4418eead918c54
CodeMonkey <louischan0325@gmail.com>
2014-01-10 23:41:03 +0800

    draft of dialogue system

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -6,9 +6,22 @@ public class DialogueManager : MonoBehaviour {
 	private Vector3 scaleVector;
 	private Vector2 scrollPosition;
 
-	private string dialogue;
-	private string speaker;
-	
+	public int characterPerSecond = 10;
+
+	private Dialogue[] dialogues;
+	private Dialogue currentDialogue;
+	private int currentLength;
+	private bool isPrinting;
+	private bool isChoosing;
+
+	void Awake() {
+		this.enabled = false;
+	}
+
+	void OnEnable() {
+		InvokeRepeating("increment", Time.time, 1.0f / characterPerSecond);
+	}
+
 	void Start() {
 		float widthRatio = Screen.width / 1920f;
 		float heightRatio = Screen.height / 1080f;
@@ -16,8 +29,6 @@ public class DialogueManager : MonoBehaviour {
 		scaleVector = new Vector3(scaleFactor, scaleFactor, 1.0f);
 		scrollPosition = Vector2.zero;
 
-		dialogue = "";
-		speaker = "";
 	}
 	
 	void OnGUI() {
@@ -28,12 +39,12 @@ public class DialogueManager : MonoBehaviour {
 		style.fontSize = 41;
 
 		GUILayout.BeginArea(new Rect(0, 740, 200, 70), GUI.skin.box);
-		GUILayout.Label(speaker, style);
+		GUILayout.Label(currentDialogue.Speaker, style);
 		GUILayout.EndArea();
 
 		GUILayout.BeginArea(new Rect(0, 810, 1980, 270), GUI.skin.box);
 		scrollPosition = GUILayout.BeginScrollView(scrollPosition, GUILayout.Width(1900), GUILayout.Height(240));
-		GUILayout.Label(dialogue, style);
+		GUILayout.Label(currentDialogue.Text.Substring(0, currentLength), style);
 		GUILayout.EndScrollView();
 		GUILayout.EndArea();
 
@@ -43,4 +54,21 @@ public class DialogueManager : MonoBehaviour {
 		GUI.matrix = backupMatrix;
 	}
 
+	public void showDialogue(Dialogue[] _dialogues) {
+		dialogues = _dialogues;
+		currentDialogue = dialogues[0];
+		currentLength = 0;
+		isPrinting = true;
+		isChoosing = false;
+		this.enabled = true;
+	}
+
+	private void increment() {
+		if (currentLength < currentDialogue.Text.Length)
+			currentLength++;
+		else {
+			isPrinting = false;
+			CancelInvoke("increment");
+		}
+	}
 }
---
+++ Assets/Scripts/DialogueOption.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueOption {
+
+	public DialogueOption() {
+		text = "null";
+		flag = "null";
+		value = -1;
+	}
+
+	public DialogueOption(string _text, string _flag, int _value) {
+		text = _text;
+		flag = _flag;
+		value = _value;
+	}
+
+	public string Text {
+		get {
+			return text;
+		}
+	}
+
+	public string Flag {
+		get {
+			return flag;
+		}
+	}
+
+	public int Value {
+		get {
+			return value;
+		}
+	}
+
+	private string text;
+	private string flag;
+	private int value;
+	
+}
