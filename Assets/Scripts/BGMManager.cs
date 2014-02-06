8969d62c9f86a4f02787936096f9b5260decc61e
kawai <louischan0325@gmail.com>
2014-02-07 01:20:26 +0800

    add a BGM manager for play bgm

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class BGMManager : MonoBehaviour {
+
+	/* reference to all BGMs used in game */
+	public AudioClip[] bgms;
+
+	public void PlayBGM(int index) {
+		if (audio.isPlaying)
+			audio.Stop();
+		audio.clip = bgms[index];
+		audio.Play();
+	}
+	
+	public void StopBGM() {
+		audio.Stop();
+	}
+
+	void Awake() {
+		DontDestroyOnLoad(transform.gameObject);
+	}
+
+	/*
+	void OnGUI() {
+		if (GUI.Button(new Rect(10, 10, 50, 50), "1")) {
+			PlayBGM(0);
+		} else if (GUI.Button(new Rect(70, 10, 50, 50), "2")) {
+			PlayBGM(1);
+		}
+	}*/
+}
