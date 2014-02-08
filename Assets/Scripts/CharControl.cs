769ecd60116a5b4e8a4c164a6824b13d7ccfd02a
kawai <louischan0325@gmail.com>
2014-02-09 00:03:43 +0800

    add a hero prefab with motion animations

--- Assets/Scripts/CharControl.cs
+++ Assets/Scripts/CharControl.cs
@@ -1,9 +1,76 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(CharacterController))]
 public class CharControl : MonoBehaviour
 {
-    
+	public float turnSpeed = 20.0f;
+	public float slowSpeed = 2.0f;
+	public float runSpeed = 5.0f;
+	public float jumpSpeed = 10.0f;
+	public float gravity = 9.81f;
+
+	private float vSpeed;
+
+	private Animator animator;
+	private CharacterController charController;
+	private HashIDs hash;
+
+	void Awake() {
+		animator = GetComponent<Animator>();
+		charController = GetComponent<CharacterController>();
+		hash = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<HashIDs>();
+	}
+
+	void Start() {
+		vSpeed = 0f;
+	}
+
+	void Update() {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+		bool slow = Input.GetButton("Slow");
+		bool jump = Input.GetButton("Jump");
+		AnimationHandle(h, v, slow);
+		Rotate(h, v, slow);
+		Move(h, v, slow, jump);
+	}
+
+	void Move(float h, float v, bool slow, bool jump) {
+		h *= slow ? slowSpeed : runSpeed;
+		v *= slow ? slowSpeed : runSpeed;
+		if (charController.isGrounded) {
+			vSpeed = jump? jumpSpeed : 0f;
+		}
+		vSpeed -= gravity * Time.deltaTime;
+		Vector3 moveDirection = new Vector3(h, vSpeed, v);
+		charController.Move(moveDirection * Time.deltaTime);
+	}
+
+	void Rotate(float h, float v, bool slow) {
+		if (!slow && (h != 0f || v != 0f)) {
+			Vector3 targetDirection = new Vector3(h, 0f, v);
+			Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+			Quaternion newRotation = Quaternion.Lerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
+			transform.rotation = newRotation;
+		}
+	}
+
+	void AnimationHandle(float h, float v, bool slow) {
+		if (!slow && (h != 0f || v != 0f)) {
+			animator.SetBool(hash.walkingBool, false);
+			animator.SetBool(hash.runningBool, true);
+		} else if (slow && (h != 0f || v != 0f)) {
+			animator.SetBool(hash.walkingBool, true);
+			animator.SetBool(hash.runningBool, false);
+		} else {
+			animator.SetBool(hash.walkingBool, false);
+			animator.SetBool(hash.runningBool, false);
+		}
+	}
+	
+    /*
     public float slowSpeed = 3.0f;
     public float walkSpeed = 5.0f;
     public float runSpeed = 10.0f;
@@ -64,4 +131,5 @@ public class CharControl : MonoBehaviour
         GUI.color = Color.black;
         GUI.Label(new Rect(0, nameSize.y * 5, nameSize.x, nameSize.y), showString);
     }
+    */
 }
---
+++ Assets/Scripts/HashIDs.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HashIDs : MonoBehaviour {
+	public int walkingBool;
+	public int runningBool;
+
+	void Awake() {
+		walkingBool = Animator.StringToHash("Walking");
+		runningBool = Animator.StringToHash("Running");
+	}
+}
