0117413b46609c0dec6b12458526bebb6e0b5596
kawaic <louischan0325@gmail.com>
2014-02-12 21:26:26 +0800

    the hero now can move in any direction with constant speed

--- Assets/Scripts/CharControl.cs
+++ Assets/Scripts/CharControl.cs
@@ -6,10 +6,10 @@ using System.Collections;
 public class CharControl : MonoBehaviour
 {
 	public float turnSpeed = 20.0f;
-	public float slowSpeed = 2.0f;
-	public float runSpeed = 5.0f;
+	public float slowSpeed = 5.0f;
+	public float runSpeed = 10.0f;
 	public float jumpSpeed = 10.0f;
-	public float gravity = 9.81f;
+	public float gravity = 20.0f;
 	private float vSpeed;
 	private Animator animator;
 	private CharacterController charController;
@@ -40,19 +40,15 @@ public class CharControl : MonoBehaviour
 
 	void Move(float h, float v, bool slow, bool jump)
 	{
-		h *= slow ? slowSpeed : runSpeed;
-		v *= slow ? slowSpeed : runSpeed;
 		if (charController.isGrounded)
 		{
 			vSpeed = jump ? jumpSpeed : 0f;
 		}
 		vSpeed -= gravity * Time.deltaTime;
-		Vector3 moveDirection = new Vector3(h, vSpeed, v);
+		Vector3 moveDirection = new Vector3(h, 0, v);
+		moveDirection = (slow ? slowSpeed : runSpeed) * moveDirection.normalized;
+		moveDirection.y = vSpeed;
 		charController.Move(moveDirection * Time.deltaTime);
-	}
-
-	void OnGUI() {
-		GUI.Label(new Rect (500, 500, 100, 100), vSpeed.ToString ());
 	}
 
 	void Rotate(float h, float v, bool slow)
@@ -82,67 +78,4 @@ public class CharControl : MonoBehaviour
 			animator.SetBool(hash.runningBool, false);
 		}
 	}
-	
-	/*
-    public float slowSpeed = 3.0f;
-    public float walkSpeed = 5.0f;
-    public float runSpeed = 10.0f;
-    public float jumpSpeed = 8.0f;
-    public float turnSpeed = 10.0f;
-    public float gravity = 20.0f;
-    private Vector3 moveDirection;
-    private float moveSpeed;
-    private CharacterController controller;
-    
-    void Awake()
-    {
-        controller = transform.gameObject.GetComponent<CharacterController>();
-    }
-    
-    void Start()
-    {
-        moveDirection = Vector3.zero;
-        moveSpeed = 0.0f;
-    }
-    
-    void Update()
-    {
-        moveDirection = new Vector3(Input.GetAxis("Horizontal"), moveDirection.y, Input.GetAxis("Vertical"));
-        
-        // Rotate to face forward
-        if (!Input.GetButton("Slow") && (Input.GetButton("Horizontal") || Input.GetButton("Vertical")))
-        {
-            Vector3 facingDirection = new Vector3(moveDirection.x, 0.0f, moveDirection.z);
-            if (facingDirection != Vector3.zero) 
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(facingDirection), turnSpeed * Time.deltaTime);
-        }
-        
-        if (controller.isGrounded)
-        {
-            moveSpeed = (Input.GetButton("Slow")) ? slowSpeed : walkSpeed;
-            moveDirection.y = 0.0f;
-            if (Input.GetButton("Jump"))
-            {
-                moveDirection.y = jumpSpeed;
-            }
-        }
-        
-        moveDirection.x *= moveSpeed;
-        moveDirection.z *= moveSpeed;
-        
-        // This is necessary because it makes controller grounded
-        moveDirection.y -= (gravity * Time.deltaTime);
-        controller.Move(moveDirection * Time.deltaTime);
-    }
-
-    void OnGUI()
-    {
-        
-        Vector2 nameSize;
-        string showString = moveDirection.ToString();
-        nameSize = GUI.skin.label.CalcSize(new GUIContent(showString));
-        GUI.color = Color.black;
-        GUI.Label(new Rect(0, nameSize.y * 5, nameSize.x, nameSize.y), showString);
-    }
-    */
 }
