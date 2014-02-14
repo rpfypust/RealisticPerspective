4b27b059c93f473aabbb780d8ebc4716ad6e257c
kawaic <louischan0325@gmail.com>
2014-02-14 20:26:09 +0800

    add a new tag and update tags.cs

--- Assets/Scripts/Tags.cs
+++ Assets/Scripts/Tags.cs
@@ -4,4 +4,6 @@ using System.Collections;
 public class Tags : MonoBehaviour {
 	public const string player = "Player";
 	public const string gameController = "GameController";
+    public const string wall = "Tag_Wall";
+    public const string stageObstacle = "Tag_StageObstacle";
 }
