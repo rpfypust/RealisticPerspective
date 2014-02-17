ef30730576f7009608ff0388c8e1a5e1e3c2ce36
kawaic <louischan0325@gmail.com>
2014-02-17 18:36:30 +0800

    add a letterbox camera and alternative stage camera

---
+++ Assets/Scripts/Camera/Letterbox.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Letterbox : MonoBehaviour {
+
+    [Range(0, 1)]
+    public float heightRatio = 0.8f;
+    
+    private Texture2D matte;
+
+    private float matteHeight;
+    private int screenHeight;
+    private int screenWidth;
+    
+    void Start() {
+        matte = new Texture2D(1, 1);
+        matte.SetPixel(0, 0, Color.black);
+        matte.Apply();
+
+        screenHeight = Screen.height;
+        screenWidth = Screen.width;
+        matteHeight = screenHeight * (1 - heightRatio) / 2;
+	}
+    
+	void OnGUI() {
+        GUI.DrawTexture(new Rect(0, 0, screenWidth, matteHeight), matte);
+        GUI.DrawTexture(new Rect(0, screenHeight - matteHeight, screenWidth, matteHeight), matte);
+	}
+}
---
+++ Assets/Scripts/Camera/StageView.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StageView : MonoBehaviour {
+
+    public Transform target;
+
+    [Range(10, 15)]
+    public float distance = 11f;
+
+    [Range(0f, Mathf.PI / 2)]
+    public float theta = 0.7853982f; // 45 degrees
+
+    public float damp = 4f;
+
+    public float zSlippage = 0f;
+
+    public void SetTarget(Transform t) {
+        target = t;
+    }
+
+    void Start() {
+        float desiredX = transform.position.x;
+        float desiredZ = transform.position.z + distance * Mathf.Cos(theta);
+        float desiredY = transform.position.y - distance * Mathf.Sin(theta);
+        transform.LookAt(new Vector3(desiredX, desiredY, desiredZ));
+    }
+	
+	void LateUpdate() {
+        if (!target)
+            return;
+        float desiredX = target.position.x;
+        float desiredZ = target.position.z - distance * Mathf.Cos(theta) + zSlippage;
+        float desiredY = target.position.y + distance * Mathf.Sin(theta);
+
+        Vector3 desiredPosition = new Vector3(desiredX, desiredY, desiredZ);
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, damp * Time.deltaTime);
+	}
+}
