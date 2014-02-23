1a5b649def576487fe83c0d15fb332286b9ce460
kawaic <louischan0325@gmail.com>
2014-02-23 21:50:23 +0800

    add extension methods to Animation component

---
+++ Assets/Scripts/Extensions/AnimationExtensions.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class AnimationExtensions {
+    public static IEnumerator WaitForFinished(this Animation animation) {
+        animation.Play();
+        yield return new WaitForSeconds(animation.clip.length);
+    }
+}
