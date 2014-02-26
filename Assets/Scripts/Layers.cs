a2a25bae67ad95194eebf95ac5e3eb9d7720b4e6
kawaic <louischan0325@gmail.com>
2014-02-26 16:45:38 +0800

    add a script for layers indexing

---
+++ Assets/Scripts/Layers.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Layers : MonoBehaviour {
+    public int player;
+
+    void Awake() {
+        player = LayerMask.NameToLayer("11_Player");
+    }
+}
