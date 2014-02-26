b44cac0d762643979cdcf4f20ec837255d5516b3
kawaic <louischan0325@gmail.com>
2014-02-26 16:49:09 +0800

    add a simple chasing monster AI the monster AI can now chase the player once the player is spotted

--- Assets/Scripts/Monster/MonsterAI.cs
+++ Assets/Scripts/Monster/MonsterAI.cs
@@ -4,16 +4,21 @@ using System.Collections;
 [RequireComponent(typeof(SphereCollider))]
 [RequireComponent(typeof(CapsuleCollider))]
 [RequireComponent(typeof(NavMeshAgent))]
-[RequireComponent(typeof(Rigidbody))]
 public class MonsterAI : MonoBehaviour {
     public float sightAngle = 120f;
-    public Vector3 bornPosition;
+    public Transform spawnPoint;
+
+    // a point representing nowhere
+    private Vector3 playerResetPosition = new Vector3(-999, -999, -999);
 
     private SphereCollider sphereCollider;
     private CapsuleCollider capsuleCollider;
     private NavMeshAgent agent;
+
     private GameObject player;
-    private bool isInSight;
+    private Layers layers;
+
+    public bool isInSight;
     private Vector3 playerLastSeenPosition;
 
     void Awake() {
@@ -21,38 +26,47 @@ public class MonsterAI : MonoBehaviour {
         capsuleCollider = GetComponent<CapsuleCollider>();
         agent = GetComponent<NavMeshAgent>();
         player = GameObject.FindGameObjectWithTag(Tags.player);
+        layers = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<Layers>();
     }
 
     void Start() {
         isInSight = false;
-        playerLastSeenPosition = new Vector3(-999, -999, -999);
-        agent.stoppingDistance = 2 * (capsuleCollider.radius + 
-                                      player.transform.parent.GetComponent<CharacterController>().radius);
+        playerLastSeenPosition = playerResetPosition;
+//        agent.stoppingDistance = 2 * (capsuleCollider.radius + 
+//                                      player.transform.parent.GetComponent<CharacterController>().radius);
+        // precious stoppingDistance should be set in inspector
+        // after experiments
     }
 
     void OnTriggerStay(Collider other) {
-        if (isHeroInSight(other)) {
-            isInSight = true;
-            playerLastSeenPosition = other.transform.position;
-        }
+        isInSight = (!isInSight) ? isHeroInSight(other) : isInSight;
+        playerLastSeenPosition = (isInSight) ? player.transform.position : playerLastSeenPosition;
     }
 
     void OnTriggerExit(Collider other) {
-        isInSight = false;
-    }
-
-    void Update() {
-        if (isInSight) {
-            agent.SetDestination(playerLastSeenPosition);
+        if (other.gameObject == player) {
+            isInSight = false;
         }
     }
 
-    // raycast is not considered yet
+    void Update() {
+        if (playerLastSeenPosition != playerResetPosition) {
+            agent.SetDestination(playerLastSeenPosition);
+        } else {
+            agent.Stop();
+        }
+    }
+
     private bool isHeroInSight(Collider other) {
         if (other.gameObject == player) {
             Vector3 direction = other.transform.position - transform.position;
             float angle = Vector3.Angle(transform.forward, direction);
-            return (angle < sightAngle * 0.5f);
+            if (angle < sightAngle * 0.5f) {
+                LayerMask mask = 1 << layers.player;
+                if (Physics.Raycast(transform.position, direction, sphereCollider.radius, mask)) {
+                    return true;
+                }
+            }
         }
         return false;
     }
