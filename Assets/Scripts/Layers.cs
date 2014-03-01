e8205c4b14b7cf2e6a62cbe9399b80d798ae53aa
kawaic <louischan0325@gmail.com>
2014-03-01 22:34:34 +0800

    add an incomplete monster spawning script

--- Assets/Scripts/Layers.cs
+++ Assets/Scripts/Layers.cs
@@ -3,8 +3,10 @@ using System.Collections;
 
 public class Layers : MonoBehaviour {
     public int player;
+    public int enemy;
 
     void Awake() {
         player = LayerMask.NameToLayer("11_Player");
+        enemy = LayerMask.NameToLayer("10_Enemy");
     }
 }
---
+++ Assets/Scripts/Monster/MonsterSpawning.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SphereCollider))]
+public class MonsterSpawning : MonoBehaviour {
+    public int maxNumMonsters = 1;
+    public float spawningRadius = 1f;
+    public GameObject monster;
+
+    private Layers layers;
+    private ArrayList monsters;
+
+	void Awake() {
+		layers = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<Layers>();
+	}
+    
+    void Start() {
+        monsters = new ArrayList(maxNumMonsters);
+    }
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.layer == layers.player) {
+			// player enters the spawning area
+            Debug.Log("player enters");
+		}
+	}
+
+	void OnTriggerExit(Collider other) {
+        if (other.gameObject.layer == layers.player) {
+            // player exits the spawning area
+            Debug.Log("player exits");
+			destoryAll();
+        } else if (other.gameObject.layer == layers.enemy) {
+            Debug.Log("monster exits");
+        }
+	}
+    
+    private Vector3 randomPosition() {
+        Vector2 p = Random.insideUnitCircle * spawningRadius;
+        Debug.Log(p.ToString());
+        return transform.TransformPoint(new Vector3(p.x, 0f, p.y));
+	}
+    
+    private Quaternion randomOrientation() {
+        Quaternion q = Quaternion.AngleAxis(Random.Range(0f, 359f), Vector3.up);
+        Debug.Log(q.ToString());
+        return q;
+	}
+
+	public bool spawn() {
+		if (monsters.Count < monsters.Capacity) {
+			monsters.Add(Instantiate(monster, randomPosition(), randomOrientation()));
+			return true;
+		}
+		return false;
+	}
+
+	public void destoryAll() {
+		foreach (GameObject m in monsters) {
+			Destroy(m);
+		}
+		monsters.Clear();
+	}
+}
