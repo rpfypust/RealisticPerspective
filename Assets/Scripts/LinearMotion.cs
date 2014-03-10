ac0e13f22e822e7523ea7faf94911145ee380cad
kawaic <louischan0325@gmail.com>
2014-03-10 20:28:05 +0800

    propose Motion class to abstract motion of bullet

---
+++ Assets/Scripts/LinearMotion.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public sealed class LinearMotion : Motion {
+
+	public Vector3 destination;
+	public float totalTime;
+
+	public void setJourney(Vector3 d, float t)
+	{
+		destination = d;
+		totalTime = t;
+	}
+
+	protected override IEnumerator moveCoroutine()
+	{
+		Vector3 des = destination;
+		float startTime = Time.time;
+		float total = totalTime;
+		Vector3 velocity = (des - transform.position) / total;
+		while (Time.time - startTime < total) {
+			rigidbody.MovePosition(transform.TransformPoint(velocity * Time.deltaTime));
+			yield return new WaitForFixedUpdate();
+		}
+	}
+}
---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class Motion : MonoBehaviour {
+
+	public void startMoving()
+	{
+		StartCoroutine("moveCoroutine");
+	}
+
+	public void stopMoving()
+	{
+		StopCoroutine("moveCoroutine");
+	}
+
+	protected abstract IEnumerator moveCoroutine();
+}
