092639ea7e4a58d2e8d8e9c454b612c3e7486ebc
kawaic <louischan0325@gmail.com>
2014-03-29 18:05:40 +0800

    Some adjustments to UFO fix the orientation of the model of UFO script is used to rotate the UFO instead of animation UFO cannot attack yet

---
+++ Assets/Scripts/Monster/UFO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFO : Monster {
+	//TODO: implement chase, attack
+	protected override void Start() {
+		base.Start();
+		agent.updateRotation = false; // UFO handles its rotation seperately
+	}
+
+	public override void chase(Vector3 destination) {
+		Vector2 direction = destination.toVector2XZ() - transform.position.toVector2XZ();
+		if (direction.magnitude > attackRange) {
+			Vector3 des = (direction.normalized * attackRange).toVector3XZ() + transform.position;
+			agent.SetDestination(des);
+		} else
+			agent.SetDestination(transform.position);
+	}
+}
---
+++ Assets/Scripts/Monster/UFORotation.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UFORotation : MonoBehaviour {
+	public float rotationSpeed = 30f;
+
+	void Update()
+	{
+		transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+	}
+}
