dc5be796be8dea69ce89f0c893bc4feae00424f1
kawaic <louischan0325@gmail.com>
2014-03-31 19:40:06 +0800

    elec stage mechanics

---
+++ Assets/Scripts/Stages/Elec/ElecStageMechanics.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using ElecSwitchColor = ElecSwitch.ElecSwitchColor;
+
+public class ElecStageMechanics : MonoBehaviour {
+
+	public GameObject[] blackDoors;
+	public GameObject[] redDoors;
+	public GameObject[] blueDoors;
+
+	private CameraManager cman;
+
+	void Awake()
+	{
+		cman = GameObject.FindGameObjectWithTag(Tags.mainCamera).GetComponent<CameraManager>();
+	}
+
+	void OnEnable()
+	{
+		ElecSwitch.OnElecSwitchPressed += SwitchHandler;
+	}
+
+	void OnDisable()
+	{
+		ElecSwitch.OnElecSwitchPressed -= SwitchHandler;
+	}
+
+	private void SwitchHandler(ElecSwitch elecSwitch, ElecSwitchColor color)
+	{
+		StartCoroutine(SwitchHandlerCoroutine(elecSwitch, color));
+	}
+
+	private IEnumerator SwitchHandlerCoroutine(ElecSwitch elecSwitch, ElecSwitchColor color)
+	{
+		elecSwitch.TurnOff();
+		cman.BeginCutScene();
+		yield return StartCoroutine(elecSwitch.animation.WaitForFinished());
+		// the switch stays pressed after the first press
+		// perhaps needs some way to revert to unpressed after the cutscene
+		Debug.Log(color);
+		yield return new WaitForSeconds(1f);
+		cman.EndCutScene();
+		elecSwitch.TurnOn();
+	}
+}
---
+++ Assets/Scripts/Stages/Elec/ElecSwitch.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(Animation))]
+public class ElecSwitch : MonoBehaviour {
+
+	public enum ElecSwitchColor {
+		RED,
+		BLUE,
+		BLACK
+	};
+
+	public delegate void ElecSwitchEvent(ElecSwitch es, ElecSwitchColor esc);
+	public static event ElecSwitchEvent OnElecSwitchPressed;
+
+	public ElecSwitchColor color;
+	private SphereCollider col;
+	private Layers layers;
+
+	void Awake() {
+		layers = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<Layers>();
+		col = GetComponent<SphereCollider>();
+	}
+
+	public void TurnOff()
+	{
+		col.enabled = false;
+	}
+
+	public void TurnOn()
+	{
+		col.enabled = true;
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject.layer == layers.player
+		    && Input.GetButton("Fire1")
+		    && OnElecSwitchPressed != null) {
+			OnElecSwitchPressed(this, color);
+		}
+	}
+}
