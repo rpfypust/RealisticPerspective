cc617bddbe0e6fa9cdc26d7d7b349612a2e072cb
kawaic <louischan0325@gmail.com>
2014-04-01 03:10:09 +0800

    the mechanics of elec stage is finished

---
+++ Assets/Scripts/Stages/Elec/ElecDoor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElecDoor : MonoBehaviour {
+
+	private BoxCollider col;
+
+	void Awake()
+	{
+		col = GetComponent<BoxCollider>();
+	}
+
+	public void Toggle(bool open)
+	{
+		AnimationState state = animation["Open"];
+
+		if (open) {
+			state.normalizedTime = 0f;
+			state.speed = 1f;
+			col.enabled = false;
+		} else {
+			state.normalizedTime = 1f;
+			state.speed = -1f;
+			col.enabled = true;
+		}
+
+		animation.Play();
+	}
+}
