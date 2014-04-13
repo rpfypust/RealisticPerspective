e401cfbd18c85409d1e744e451e01a6d9471e183
suwaki <jo.ak97@yahoo.com.hk>
2014-04-13 23:35:22 +0800

    -Added a boss_atrium.unity for final boss stage

--- Assets/Scripts/BulletPattern/Boss_Phoenix.cs
+++ Assets/Scripts/BulletPattern/Boss_Phoenix.cs
@@ -24,19 +24,21 @@ public class Boss_Phoenix : MonoBehaviour
     private float localStartTime = 0.0f;
     private float countdownUntil = 0.0f;
     private Vector3 StageRefPoint;
-    private BossStatus status;
-    private Transform boss;
+    //private BossStatus status;
+    //private Transform boss;
     private int bossState;
+
+    private GameObject[] BossObject_Phoenix_X = new GameObject[13];
     
     void Awake()
     {
         startTime = Time.time;
         bossState = 0;
-        status = transform.parent.gameObject.GetComponent<BossStatus>();
-        boss = transform.parent;
+        //status = transform.parent.gameObject.GetComponent<BossStatus>();
+        //boss = transform.parent;
         StageRefPoint = GameObject.FindGameObjectWithTag("StageRefPoint").transform.position;
         
-        status.isInvicible = true;
+        //status.isInvicible = true;
     }
     
     void FixedUpdate()
@@ -44,6 +46,66 @@ public class Boss_Phoenix : MonoBehaviour
         switch (bossState)
         {
             case 0:
+                int angle = 0;
+                Vector3 tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[1] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_1, tempV, new Quaternion(0, 0, 0, 0));
+
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[2] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_2, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[3] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_3, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[4] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_4, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[5] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_5, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[6] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_6, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[7] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_7, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[8] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_8, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[9] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_9, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[10] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_10, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[11] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_11, tempV, new Quaternion(0, 0, 0, 0));
+                
+                angle += 30;
+                tempV = StageRefPoint+new Vector3(30.0f*Mathf.Sin(angle*Mathf.Deg2Rad),3.5f,30.0f*Mathf.Cos(angle*Mathf.Deg2Rad));
+                BossObject_Phoenix_X[12] = 
+                    (GameObject)Instantiate(BossObject_Phoenix_12, tempV, new Quaternion(0, 0, 0, 0));
+                bossState++;
                 break;
         }
     }
