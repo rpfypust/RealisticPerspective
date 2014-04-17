8767ba9d7102099e7539fda5bc05e0b690381aac
kawaic <louischan0325@gmail.com>
2014-04-18 01:54:36 +0800

    game controller can change scene with fading in and out

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -5,6 +5,9 @@ public sealed class GameController : MonoBehaviour {
 
 	private static GameController instance;
 
+	private Fader fader;
+	private GUIManager gman;
+
 	void Awake()
 	{
 		if (instance == null) {
@@ -13,5 +16,22 @@ public sealed class GameController : MonoBehaviour {
 		} else {
 			Destroy(gameObject);
 		}
+
+		fader = GetComponent<Fader>();
+		gman = GetComponent<GUIManager>();
+	}
+
+	public void LoadLevel(int index)
+	{
+		StartCoroutine(LoadLevelCoroutine(index));
+	}
+
+	private IEnumerator LoadLevelCoroutine(int index)
+	{
+		gman.register(fader);
+		yield return StartCoroutine(fader.Fade(0f, 1f, 1f));
+		Application.LoadLevel(index);
+		yield return StartCoroutine(fader.Fade(1f, 0f, 1f));
+		gman.unregister(fader);
 	}
 }
