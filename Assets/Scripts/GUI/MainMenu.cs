28ad6a28760f061891beb17900cd565063a86d3d
kawaic <louischan0325@gmail.com>
2014-04-19 02:54:11 +0800

    remake title

---
+++ Assets/Scripts/GUI/MainMenu.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+public class MainMenu : TitleScreen, IDrawable {
+
+	private Rect buttonRect;
+	private GameController gamecon;
+	private GUIManager gman;
+
+	protected override void Awake()
+	{
+		base.Awake();
+
+		gamecon = GameObject.FindGameObjectWithTag(Tags.gameController)
+			.GetComponent<GameController>();
+		gman = GameObject.FindGameObjectWithTag(Tags.gameController)
+			.GetComponent<GUIManager>();
+
+		buttonRect = new Rect(640, 216, 640, 648);
+	}
+
+	private void OnEnable()
+	{
+		gman.register(this);
+	}
+
+	private void OnDisable()
+	{
+		gman.unregister(this);
+	}
+
+	public void DrawOnGUI()
+	{
+		base.DrawOnGUI();
+		GUIStyle style = new GUIStyle(GUI.skin.button);
+		style.fontSize = 43;
+
+		GUILayout.BeginArea(buttonRect);
+		GUILayout.BeginVertical();
+		if (GUILayout.Button("New Game", style, GUILayout.ExpandHeight(true))) {
+			// load the first scene
+			Debug.Log("new game");
+			//gamecon.LoadLevel(0);
+		}
+		if (GUILayout.Button("Continue", style, GUILayout.ExpandHeight(true))) {
+			// get current progress from permanent storage
+			// load the atrium scene
+			Debug.Log("continue");
+			//gamecon.LoadLevel(0);
+		}
+		if (GUILayout.Button("Exit", style, GUILayout.ExpandHeight(true))) {
+			Application.Quit();
+		}
+		GUILayout.EndVertical();
+		GUILayout.EndArea();
+	}
+}
--- Assets/Scripts/GUI/TitleScreen.cs
+++ Assets/Scripts/GUI/TitleScreen.cs
@@ -1,70 +1,24 @@
-﻿//using UnityEngine;
-//using System.Collections;
-//
-//public class TitleScreen : MonoBehaviour {
-//	
-//	public Texture2D backgroundImage;
-//	
-//	private Rect buttonArea;
-//	private Vector3 scaleVector;
-//	private int fontSize;
-//	
-//	private Fader fader;
-//	
-//	void Awake() {
-//		fader = GameObject.Find("ScreenFader").GetComponent<Fader>();
-//	}
-//	
-//	void Start() {
-//		// calculate the scale vector
-//		float widthRatio = Screen.width / 1920f;
-//		float heightRatio = Screen.height / 1080f;
-//		float scaleFactor = (widthRatio > heightRatio) ? heightRatio : widthRatio;
-//		scaleVector = new Vector3(scaleFactor, scaleFactor, 1f);
-//		
-//		// initialize the button area
-//		int areaWidth = 640;
-//		int areaHeight = 648;
-//		int areaX = 640;
-//		int areaY = 216;
-//		buttonArea = new Rect(areaX, areaY, areaWidth, areaHeight);
-//		
-//		// initialize the font style
-//		fontSize = 43;
-//	}
-//	
-//	void OnGUI() {
-//		// backup the matrix
-//		Matrix4x4 backupMatrix = GUI.matrix;
-//		
-//		GUI.matrix = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, scaleVector);
-//		
-//		// Draw the background image
-//		GUI.DrawTexture(new Rect(0, 0, 1920, 1080), backgroundImage, ScaleMode.StretchToFill);
-//		
-//		GUIStyle buttonStyle = new GUIStyle(GUI.skin.button);
-//		buttonStyle.fontSize = fontSize;
-//		
-//		GUILayout.BeginArea(buttonArea);
-//		GUILayout.BeginVertical();
-//		if (GUILayout.Button("Play Game", buttonStyle, GUILayout.ExpandHeight(true))) {
-//			fader.LoadLevel(1);
-//			//Application.LoadLevel("testing_use");
-//		}
-//		if (GUILayout.Button("Load Game", buttonStyle, GUILayout.ExpandHeight(true))) {
-//			fader.LoadLevel(2);
-//			Debug.Log("game loaded");
-//		}
-//		if (GUILayout.Button("Options", buttonStyle, GUILayout.ExpandHeight(true))) {
-//			Debug.Log("options");
-//		}
-//		if (GUILayout.Button("Quit", buttonStyle, GUILayout.ExpandHeight(true))) {
-//			Application.Quit();
-//		}
-//		GUILayout.EndVertical();
-//		GUILayout.EndArea();
-//		
-//		// restore the matrix
-//		GUI.matrix = backupMatrix;
-//	}
-//}
+﻿using UnityEngine;
+
+public class TitleScreen : MonoBehaviour {
+
+	private Texture2D bgimg;
+	private float width;
+	private float height;
+
+	protected virtual void Awake()
+	{
+		bgimg = Resources.Load<Texture2D>("cover_1920x1080");
+	}
+
+	protected virtual void Start()
+	{
+		width = GUIManager.width;
+		height = GUIManager.height;
+	}
+
+	public void DrawOnGUI()
+	{
+		GUI.DrawTexture(new Rect(0, 0, width, height), bgimg);
+	}
+}
