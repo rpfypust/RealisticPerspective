2609f542b811225e3f995617edc91b6a6ea9bbdd
kawaic <louischan0325@gmail.com>
2014-04-21 22:10:57 +0800

    set BGM as 2D sounds add BGM manager to game controller

--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -1,33 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-[RequireComponent(typeof(AudioSource))]
 public class BGMManager : MonoBehaviour {
 
 	/* references to all BGMs */
 	public AudioClip[] bgms;
+	public Vector2[] pairs;
 
 	public void PlayBGM(int index) {
 		if (audio.isPlaying)
-			audio.Stop();
-		audio.clip = bgms[index];
-		audio.Play();
+			StopBGM();
+		StartCoroutine(LoopCoroutine(index));
 	}
 	
 	public void StopBGM() {
+		StopAllCoroutines();
 		audio.Stop();
 	}
 
-	void Awake() {
-		DontDestroyOnLoad(transform.gameObject);
+	private IEnumerator LoopCoroutine(int index)
+	{
+		audio.clip = bgms[index];
+		audio.Play();
+		yield return new WaitForSeconds(pairs[index].y);
+		while (true) {
+			audio.time = pairs[index].x;
+			yield return new WaitForSeconds(pairs[index].y - pairs[index].x);
+		}
 	}
 
-	/*
-	void OnGUI() {
-		if (GUI.Button(new Rect(10, 10, 50, 50), "1")) {
-			PlayBGM(0);
-		} else if (GUI.Button(new Rect(70, 10, 50, 50), "2")) {
-			PlayBGM(1);
-		}
-	}*/
+
+
+
+
+//	void OnGUI() {
+//		if (GUI.Button(new Rect(10, 10, 50, 50), "1"))
+//			PlayBGM(0);
+//		else if (GUI.Button(new Rect(10, 100, 50, 50), "2"))
+//			PlayBGM(1);
+//	}
 }
